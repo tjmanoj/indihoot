ed220ecd07d461c2d493c5bdfb35cba85c6c544a
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2025-09-01 00:00:05 +0530

    Create Continuous Subarray sum.java

---
+++ Arrays/Continuous Subarray sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int prefixSum = 0;
+        hashMap.put(prefixSum,0);
+
+        for(int i=0; i< nums.length; i++)
+        {
+            int curNum = nums[i];
+            prefixSum = (prefixSum + curNum) % k;
+
+            if(hashMap.get(prefixSum) != null)
+            {
+                int index = hashMap.get(prefixSum);
+                if(i+1 - index >= 2) return true;
+            }
+            else
+            {
+                hashMap.put(prefixSum, i+1);
+            }
+        }
+        return false;
+    }
+}
