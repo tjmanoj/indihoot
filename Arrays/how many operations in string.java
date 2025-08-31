a626e58f11ac59e69af26a94de20e3f6e119e435
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2025-08-31 22:20:35 +0530

    Create how many operations in string.java

---
+++ Arrays/how many operations in string.java
@@ -0,0 +1,25 @@
+public class Problem266A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        String s = sc.next();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c: s.toCharArray())
+        {
+          int len = sb.length();
+          if(len == 0 || sb.charAt(len - 1) != c)
+          {
+            sb.append(c);
+          }
+          else 
+          {
+            sb.deleteCharAt(len - 1);
+            sb.append(c);
+          }
+        }
+        System.out.println(n - sb.length());
+    }
+}
