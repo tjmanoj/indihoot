6645e19b43f3e27a91bc1f92c317cdbb9bfec0e3
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2025-08-31 20:40:25 +0530

    Create sort array by parity.java

---
+++ Arrays/sort array by parity.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums;
+        int left = 0;
+        int right = 1;
+        while(right < n && (nums[left] & 1) == 0)
+        {
+            left++;
+            right++;
+        }
+
+        while(right < n)
+        {
+            if((nums[right] & 1) == 0)
+            {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+            right++;
+        }
+        return nums;
+    }
+}
