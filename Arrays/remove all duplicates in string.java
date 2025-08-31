3654c637b1b7742aae5f6c30ba2eb86bd73807d9
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2025-08-31 19:42:18 +0530

    Update remove all duplicates in string.java

--- Arrays/remove all duplicates in string.java
+++ Arrays/remove all duplicates in string.java
@@ -1,5 +1,25 @@
 class Solution {
     public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c: s.toCharArray())
+        {
+            int len = sb.length();
+            if(len == 0 || sb.charAt(len-1) != c)
+            {
+                sb.append(c);
+            }
+            else
+            {
+                sb.deleteCharAt(len-1);
+            }
+        }
+        return sb.toString();
+    }
+}
+
+class Solution {
+    public String removeDuplicates(String s) {
         Stack<Character> stack = new Stack<>();
         String result = "";
         for(int i=s.length() - 1; i >= 0; i--)
