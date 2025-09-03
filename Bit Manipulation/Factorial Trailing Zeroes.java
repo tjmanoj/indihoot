e1d9c8bb579e72bf507b3d89d6481494c52deab4
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2025-09-03 22:29:39 +0530

    Update Factorial Trailing Zeroes.java

--- Bit Manipulation/Factorial Trailing Zeroes.java
+++ Bit Manipulation/Factorial Trailing Zeroes.java
@@ -1,6 +1,6 @@
 // Hint: need to find how many times 5 inside the given number.
-// Since 5 must present in order to have 0 in factorial multiplication
-
+// Since 5 x 2 will give a trailing zero. For example 15x14x13x12x11x10x9x8x7x6x5x4x3x2x1 (Here from 1 every 5th position has a 5 in multiples)
+// So if we find how many 5's thats the result.
 class Solution {
     public int trailingZeroes(int n) {
         if(n < 5)
